2386a6c915f864247f12bb33c2b84a7cbc701120
Mohammad Ali Bazzazi <81759694+bazzazi@users.noreply.github.com>
2021-06-29 21:07:31 +0430

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 from selenium import webdriver
 import time
 
-# Instialize driver and path to chrome driver location
-driver = webdriver.Chrome("chromedriver")
+# Initialize driver and path to chrome driver location
+driver = webdriver.Chrome("PATH")  # Replace your chrome driver path with PATH without .exe extension
 
 # Get insta site
 driver.get("https://www.instagram.com/")
@@ -13,8 +13,8 @@ time.sleep(5)
 
 # Import info.py (which includes your username and password)
 import info
-username = info.USERNAME
 
+username = info.USERNAME
 password = info.PASSWORD
 
 # Entering username and password to fields
@@ -24,6 +24,7 @@ driver.find_element_by_xpath("""//*[@id="loginForm"]/div/div[2]/div/label/input"
 # Press Login Button
 driver.find_element_by_xpath("""//*[@id="loginForm"]/div/div[3]/button/div""").click()
 
+# Pause For 15 sec
 time.sleep(15)
 
 
@@ -33,6 +34,7 @@ driver.get("https://www.instagram.com/your account name")
 # Click on following link
 driver.find_element_by_partial_link_text("following").click()
 
+# Pause For 5 sec
 time.sleep(5)
 
 
@@ -43,6 +45,7 @@ for i in range(0,20):
     # Click on Unfollow Button
     driver.find_element_by_xpath("//button[text()='Unfollow']").click()
 
+    # Pause For 3 sec
     time.sleep(3)
 
 
